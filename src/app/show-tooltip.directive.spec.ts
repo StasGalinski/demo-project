ed08d95d93bd7ecee2514b57858d129c82f202e3
StasGalinski <stas.galinski@gmail.com>
2023-08-15 12:10:17 +0300

    Small change

--- src/app/show-tooltip.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowTooltipDirective } from './show-tooltip.directive';
-
-describe('ShowTooltipDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowTooltipDirective();
-    expect(directive).toBeTruthy();
-  });
-});
